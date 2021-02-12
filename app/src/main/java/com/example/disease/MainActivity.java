24107d8a31838668965b6b7a6bac12f57c13ed55
stevenkreutzer <steven.kreutzer@yahoo.com>
2021-02-12 11:47:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/disease/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.disease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.disease.models.State;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    String url = "https://corona.lmao.ninja/v2/states?sort=&yesterday=";
+
+    List<State> states;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        states = new ArrayList<State>();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, json.toString());
+                Log.d(TAG, json.jsonArray.toString());
+                try {
+                    states.addAll(State.fromJsonArray(json.jsonArray));
+                    Log.d(TAG, "Size: " + states.size());
+                    Collections.sort(states, new Comparator<State>() {
+                        @Override
+                        public int compare(State x, State y) {
+                            return y.casesPerOneMillion - x.casesPerOneMillion;
+                        }
+                    });
+                    Log.d(TAG, "Top State: " + states.get(0).state + " " + (states.get(0).casesPerOneMillion/10000)+"%");
+                    Log.d(TAG, "Tenth State: " + states.get(1).state + " " + (states.get(10).casesPerOneMillion/10000)+"%");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "Error Response", throwable);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/disease/models/State.java
@@ -0,0 +1,45 @@
+package com.example.disease.models;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class State {
+    public String state;
+    int cases;
+    public int casesPerOneMillion;
+
+    public static final String TAG = "State";
+
+    public State(JSONObject jsonObject) throws JSONException {
+        state = jsonObject.getString("state");
+        cases = jsonObject.getInt("cases");
+        casesPerOneMillion = jsonObject.getInt("casesPerOneMillion");
+
+        Log.d(TAG, toString());
+    }
+
+    @Override
+    public String toString() {
+        return "State{" +
+                "state='" + state + '\'' +
+                ", cases=" + cases +
+                ", casesPerOneMillion=" + casesPerOneMillion +
+                '}';
+    }
+
+    public static List<State> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<State> states = new ArrayList<State>();
+
+        for (int i=0; i<jsonArray.length(); i++)
+            states.add(new State(jsonArray.getJSONObject(i)));
+
+        return states;
+    }
+
+}
